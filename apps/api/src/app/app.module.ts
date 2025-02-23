e686cd4fad910eb6f2e20895adc927921a4b83f4
Vimu26 <akalankavimukthi2@gmail.com>
2025-02-23 15:20:34 +0530

    connected to local mongo db database

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,9 +1,32 @@
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const uri = config.get<string>('DB_URL');
+        if (!uri) {
+          throw new Error(
+            'DB_URL is not defined in the environment variables.'
+          );
+        }
+        Logger.log(`🚀 DB Connected in url ${uri}`);
+        return { uri };
+      },
+    }),
+    HttpModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
