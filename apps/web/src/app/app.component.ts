36720f47ed9b921a5dd9b5ddc0cab2ea39d6e12f
Vimu26 <akalankavimukthi2@gmail.com>
2025-02-24 18:59:26 +0530

    footer component

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from './material.module';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
-  imports: [RouterModule,MaterialModule],
+  imports: [RouterModule,MaterialModule,FooterComponent],
   standalone:true,
   selector: 'app-root',
   templateUrl: './app.component.html',
