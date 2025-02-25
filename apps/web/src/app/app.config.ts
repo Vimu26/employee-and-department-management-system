564bcfa870d14c9995b6a45eee90d03f4f1b4017
Vimu26 <akalankavimukthi2@gmail.com>
2025-02-25 13:40:29 +0530

    added animations

--- apps/web/src/app/app.config.ts
+++ apps/web/src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
@@ -8,5 +9,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
     provideHttpClient(withInterceptorsFromDi()),
+    provideAnimations()
   ],
 };
