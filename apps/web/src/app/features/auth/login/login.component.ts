008e7231896f80e334486d040855454b1e02fbf7
Vimu26 <akalankavimukthi2@gmail.com>
2025-02-25 15:58:10 +0530

    login page ui and footer fixes

--- apps/web/src/app/features/auth/login/login.component.ts
+++ apps/web/src/app/features/auth/login/login.component.ts
@@ -1,11 +1,36 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormGroup,
+  Validators,
+  FormControl,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MaterialModule } from '../../../material.module';
+import { RouterModule } from '@angular/router';
 
+export interface LoginFormGroup {
+  username: FormControl<string | null>;
+  password: FormControl<string | null>;
+}
 @Component({
   selector: 'app-login',
-  imports: [CommonModule],
+  imports: [CommonModule, MaterialModule, ReactiveFormsModule, RouterModule],
   standalone: true,
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm = new FormGroup<LoginFormGroup>({
+    username: new FormControl<string | null>('', 
+      Validators.required
+    ),
+    password: new FormControl<string | null>('', Validators.required),
+  });
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
+  }
+}
