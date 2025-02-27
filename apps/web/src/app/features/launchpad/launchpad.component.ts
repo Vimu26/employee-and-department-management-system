3e04df5a8f77c4b5a12b296354d3d9cc738ef002
Vimu26 <akalankavimukthi2@gmail.com>
2025-02-27 10:28:59 +0530

    sample launchpad created

--- apps/web/src/app/features/launchpad/launchpad.component.ts
+++ apps/web/src/app/features/launchpad/launchpad.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../../material.module';
 
 @Component({
   selector: 'app-launchpad',
-  imports: [CommonModule],
+  imports: [CommonModule, MaterialModule],
   standalone: true,
   templateUrl: './launchpad.component.html',
   styleUrl: './launchpad.component.scss',
 })
-export class LaunchpadComponent {}
+export class LaunchpadComponent implements OnInit {
+  name = '';
+
+  ngOnInit(): void {
+    const userData = localStorage.getItem('user')
+      ? JSON.parse(localStorage.getItem('user')!)
+      : null;
+    if (userData) {
+      this.name = userData?.name?.first_name + ' ' + userData?.name?.last_name;
+    }
+  }
+}
