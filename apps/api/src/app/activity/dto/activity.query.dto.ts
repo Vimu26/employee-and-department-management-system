6568fdbd15ef195d7ce413fe384a8eb41f1142c6
Vimu26 <akalankavimukthi2@gmail.com>
2025-02-27 17:31:06 +0530

    activity query dto

--- apps/api/src/app/activity/dto/activity.query.dto.ts
+++ apps/api/src/app/activity/dto/activity.query.dto.ts
@@ -3,15 +3,7 @@ import { CreateActivityDto } from './create-activity.dto';
 import { IsOptional, IsString, IsNumber } from 'class-validator';
 
 export class ActivityLogQueryDto extends PartialType(CreateActivityDto) {
-  @IsOptional()
-  @IsString()
   _id?: string;
-
-  @IsOptional()
-  @IsNumber()
-  limit?: number;
-
-  @IsOptional()
-  @IsNumber()
-  skip?: number;
+  size?: number;
+  start?: number;
 }
